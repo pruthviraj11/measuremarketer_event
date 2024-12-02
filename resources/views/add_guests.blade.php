eb7e737e51c9c925bf51159d7c552d4faf151b5d
pradipdan <pradip12345.pv@gmail.com>
2024-12-02 19:45:49 +0530

    changes -vishhhs

--- resources/views/add_guests.blade.php
+++ resources/views/add_guests.blade.php
@@ -21,7 +21,8 @@
                                     <!-- Guests Container -->
                                     <div class="guests-container">
                                         <div class="guest-form" id="guest-0">
-                                            <div class="form-row"> <!-- Use form-row for grid system -->
+                                            <div class="form-row d-flex  align-items-center">
+                                                <!-- Use form-row for grid system -->
                                                 <div class="form-group col-md-3">
                                                     <label for="name">Name</label>
                                                     <input type="text" name="guests[0][name]" class="form-control"
@@ -38,6 +39,19 @@
                                                 <div class="col-md-3">
                                                     <button type="button" class="btn btn-danger remove-guest"
                                                         style="display:none; margin-top: 10px;">Remove</button>
+                                                    <button type="button" style="margin-top: 13px;"
+                                                        class="btn btn-secondary add-more"><svg
+                                                            xmlns="http://www.w3.org/2000/svg" width="16"
+                                                            height="16" viewBox="0 0 24 24" fill="none"
+                                                            stroke="currentColor" stroke-width="2"
+                                                            stroke-linecap="round" stroke-linejoin="round"
+                                                            class="feather feather-plus-circle">
+                                                            <circle cx="12" cy="12" r="10"></circle>
+                                                            <line x1="12" y1="8" x2="12"
+                                                                y2="16"></line>
+                                                            <line x1="8" y1="12" x2="16"
+                                                                y2="12"></line>
+                                                        </svg></button>
                                                 </div>
                                             </div>
 
@@ -47,9 +61,11 @@
                                     </div>
 
                                     <!-- Add More Button and Submit Button -->
-                                    <button type="button" class="btn btn-secondary add-more add_more_btn">Add
-                                        More</button>
-                                    <button type="submit" class="btn btn-primary save_guest_btn">Save Guests</button>
+
+                                    <div class="text-center">
+                                        <button type="submit" class="btn btn-primary save_guest_btn">Save
+                                            Guests</button>
+                                    </div>
                                 </form>
 
                             </div>
--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -222,7 +222,10 @@
                     <input type="text" name="guests[${guestIndex}][phone]" class="form-control">
                 </div>
                 <div class="form-group col-md-3">
-                    <button type="button" class="btn btn-danger remove-guest" style="margin-top: 10px;"> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg> </button>
+
+                    <button type="button" class="btn btn-danger remove-guest" style="margin-top: 10px;">
+
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg> </button>
                 </div>
             </div>
            <!-- Add margin-top for spacing -->
@@ -232,6 +235,7 @@
 
          // Make the Remove button visible after adding the form
          document.querySelector(`#guest-${guestIndex} .remove-guest`).style.display = 'inline-block';
+         document.querySelector(`. add-more`).style.display = 'inline-block';
 
          guestIndex++;
      });
