c7e26609012c3ecd4e51741a65ee5db129725a00
Vipul <vcprajapati.mscit@gmail.com>
2024-12-11 20:18:15 +0530

    change update profile

--- app/Http/Controllers/EventRegisterController.php
+++ app/Http/Controllers/EventRegisterController.php
@@ -219,7 +219,7 @@ class EventRegisterController extends Controller
                         '<p>' . $event->start_date . '</p>' .
                         '<p>' . $event->start_time . '</p>' .
                         '<p>' . $event->address . '</p>' .
-                        '<a href="https://maps.app.goo.gl/oskv54K7JdwqkTGg9" target="_blank"> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-map-pin"><path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path><circle cx="12" cy="10" r="3"></circle></svg></p>' .
+                        '<a href="https://maps.app.goo.gl/oskv54K7JdwqkTGg9" target="_blank"> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="#e91a83" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-map-pin"><path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path><circle cx="12" cy="10" r="3"></circle></svg></p>' .
 
 
                         '</div>';
